45c424620fdaf48b241edc3104ca2fe3c53aced8
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-01-05 18:00:08 +0100

    step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,54 +1,54 @@
 const App = () => {
-  const course = "Half Stack application development";
+  const course = {
+    name: "Half Stack application development",
 
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+
+      {
+        name: "State of component",
+        exercises: 14,
+      },
+    ],
   };
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-
-  const part3 = {
-    name: "State of component",
-    exercises: 14,
-  };
-
+  console.log(course.parts);
   return (
     <div>
-      <Header course={course} />
+      <Header course={course.name} />
       <Content
-        part1={part1.name}
-        exercises1={part1.exercises}
-        part2={part2.name}
-        exercises2={part2.exercises}
-        part3={part3.name}
-        exercises3={part3.exercises}
+        parts={course.parts}
+        // exercises1={parts.map((p) => p.exercises)}
       />
 
-      <Total a={part1.exercises} b={part2.exercises} c={part3.exercises} />
+      <Total
+        a={course.parts[0].exercises}
+        b={course.parts[1].exercises}
+        c={course.parts[2].exercises}
+      />
     </div>
   );
 };
 export default App;
 
 const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
+  return <h1>{props.course}</h1>;
 };
 
 const Content = (props) => {
   return (
-    <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
-    </>
+    <ul>
+      {props.parts.map((part, idx) => (
+        <Part part={part.name} exercises={part.exercises} key={idx} />
+      ))}
+    </ul>
   );
 };
 
@@ -58,10 +58,8 @@ const Total = (props) => {
 
 const Part = (props) => {
   return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
+    <li>
+      {props.part} {props.exercises}
+    </li>
   );
 };
